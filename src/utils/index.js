64d60afff0defbef0c835f9c75fdcd895abdf9a7
trombonist76 <mahmut.hg@gmail.com>
2022-12-26 20:16:37 +0300

    [Add] index utils added

---
+++ src/utils/index.js
@@ -0,0 +1,22 @@
+import { toRaw } from "vue"
+
+export function isInArrayById(array, id){
+    const isInArray = array.some(item => item.id === id)
+    return isInArray
+}
+
+function cityEmitFields (city) {
+    const copiedCity = structuredClone(toRaw(city))
+    delete copiedCity.path
+    return copiedCity
+}
+
+export function prepareToEmit (cityData) {
+    if( Array.isArray(cityData) ){
+        const emitArray = cityData.map(city => cityEmitFields(city))
+        return emitArray
+    }
+    //else
+    const emitObj = cityEmitFields(cityData)
+    return emitObj
+}
