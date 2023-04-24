4232327f085f6be3503b6388f35f17b64a037697
trombonist76 <mahmut.hg@gmail.com>
2023-04-24 22:07:23 +0300

    [Build] Configure project for npm publishing.

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,28 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/components/index.js'),
+      name: 'TurkeyMap',
+      fileName: 'turkey-map'
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue'
+        }
+      }
     }
   }
 })
