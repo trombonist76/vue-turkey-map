f61c0b751a8189c87c44664d599db94204029073
trombonist76 <mahmut.hg@gmail.com>
2023-04-25 10:52:46 +0300

    [Build] Add PrimeVue UI library to project dependencies.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import { createApp } from 'vue'
+import PrimeVue from 'primevue/config';
 import App from './App.vue'
 
+import "primevue/resources/themes/lara-light-indigo/theme.css";     
+import "primevue/resources/primevue.min.css";
 import './assets/main.css'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(PrimeVue);
+app.mount('#app')
+
