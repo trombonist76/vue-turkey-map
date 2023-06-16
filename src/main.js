030e7ac9415331a6b87a09bcbddcc832baa1d9d4
mahmuth <mgol@protel.com.tr>
2023-06-16 15:24:33 +0300

    [Build] Add PrimeIcons library and Toast provider to the project.

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import { createApp } from 'vue'
 import PrimeVue from 'primevue/config';
+import ToastService from 'primevue/toastservice';
 import App from './App.vue'
 
 import "primevue/resources/themes/lara-light-indigo/theme.css";     
 import "primevue/resources/primevue.min.css";
+import 'primeicons/primeicons.css';
 import './assets/main.css'
 
 const app = createApp(App);
 app.use(PrimeVue);
+app.use(ToastService);
 app.mount('#app')
 
